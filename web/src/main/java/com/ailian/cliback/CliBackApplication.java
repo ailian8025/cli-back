866fe2e14f47afb469c702432a560b004778db7e
ailian <ailian8025@outlook.com>
2021-02-07 10:13:34 +0800

    Initial commit

---
+++ web/src/main/java/com/ailian/cliback/CliBackApplication.java
@@ -0,0 +1,13 @@
+package com.ailian.cliback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CliBackApplication.class, args);
+    }
+
+}
---
+++ web/src/test/java/com/ailian/cliback/CliBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ailian.cliback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
